7001d96646de37b1f5e974355e05471479037dee
marko <marko.pavicevic@outlook.com>
2026-05-23 17:51:04 -0700

    add markdown support

--- src/app/solve/[problemId]/page.js
+++ src/app/solve/[problemId]/page.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { CodebookDatabaseAPI } from "@/lib/db";
 import ProblemClient from "./ProblemClient";
+import Markdown from "react-markdown";
+import rehypeSanitize from "rehype-sanitize";
 
 export default async function SolvePage({ params }) {
   const { problemId } = await params;
-
   const problem = await CodebookDatabaseAPI.getProblemById(problemId);
-
   const problemCreator = await CodebookDatabaseAPI.getUserById(problem.userId);
 
   if (!problem) {
     return <h1>Problem {problemId} not found</h1>;
   }
 
-  console.log("DEBUG CREATOR DETAILS:", problemCreator);
+  const renderedMarkdown = (
+    <div className="problem-markdown text-sm">
+      <Markdown rehypePlugins={[rehypeSanitize]}>{problem.description}</Markdown>
+    </div>
+  );
 
-  return <ProblemClient problem={problem} problemCreator={problemCreator} />;
+  return (
+    <ProblemClient
+      problem={problem}
+      problemCreator={problemCreator}
+      description={renderedMarkdown}
+    />
+  );
 }
