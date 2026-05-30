5d2b8eef01972527b37113e736d89369c9666769
marko <marko.pavicevic@outlook.com>
2026-05-29 21:19:08 -0700

    add state check for submit button

--- src/app/solve/[problemId]/ProblemClient.js
+++ src/app/solve/[problemId]/ProblemClient.js
@@ -79,14 +79,17 @@ export default function ProblemClient({
     if (!editorRef.current) return;
     const code = editorRef.current.getValue();
 
-    setStatus("Submitting code...");
+    setStatus("submitting");
     const submissionId = await saveCode(problem.id, code);
+    toast.success("Code submitted!");
 
-    setStatus("Running tests...");
+    setStatus("running");
     const data = await runCode(problem.problemId || problem.id, language, code);
 
     setResults(data);
-    setStatus("Done");
+    if (data.verdict === "Accepted") toast.success("Accepted");
+    else toast.error("Wrong Answer");
+    setStatus("done");
   };
 
   const handleReset = async (value) => {
@@ -385,23 +388,33 @@ export default function ProblemClient({
               <button
                 type="submit"
                 onClick={handleSubmit}
-                className="px-16 py-1.5 w-full rounded text-sm font-bold bg-monaco-mid text-green-500 hover:bg-green-700 hover:text-monaco-txt transition-colors cursor-pointer flex items-center justify-center"
+                disabled={status !== "" && status !== "done"}
+                className="px-16 py-1.5 w-full rounded text-sm font-bold bg-monaco-mid text-green-500 hover:bg-green-700 hover:text-monaco-txt transition-colors cursor-pointer flex items-center justify-center disabled:cursor-not-allowed disabled:bg-yellow-600 disabled:text-monaco-txt"
               >
-                <svg viewBox="0 0 500 420" className="w-5 h-5 fill-current">
-                  <g>
-                    <path
-                      d="M344.058,207.506c-16.568,0-30,13.432-30,30v76.609h-254v-76.609c0-16.568-13.432-30-30-30c-16.568,0-30,13.432-30,30
+                {status !== "" && status !== "done" ? (
+                  ""
+                ) : (
+                  <svg viewBox="0 0 500 420" className="w-5 h-5 fill-current">
+                    <g>
+                      <path
+                        d="M344.058,207.506c-16.568,0-30,13.432-30,30v76.609h-254v-76.609c0-16.568-13.432-30-30-30c-16.568,0-30,13.432-30,30
 		v106.609c0,16.568,13.432,30,30,30h314c16.568,0,30-13.432,30-30V237.506C374.058,220.938,360.626,207.506,344.058,207.506z"
-                    />
-                    <path
-                      d="M123.57,135.915l33.488-33.488v111.775c0,16.568,13.432,30,30,30c16.568,0,30-13.432,30-30V102.426l33.488,33.488
+                      />
+                      <path
+                        d="M123.57,135.915l33.488-33.488v111.775c0,16.568,13.432,30,30,30c16.568,0,30-13.432,30-30V102.426l33.488,33.488
 		c5.857,5.858,13.535,8.787,21.213,8.787c7.678,0,15.355-2.929,21.213-8.787c11.716-11.716,11.716-30.71,0-42.426L208.271,8.788
 		c-11.715-11.717-30.711-11.717-42.426,0L81.144,93.489c-11.716,11.716-11.716,30.71,0,42.426
 		C92.859,147.631,111.855,147.631,123.57,135.915z"
-                    />
-                  </g>
-                </svg>
-                <span>Submit</span>
+                      />
+                    </g>
+                  </svg>
+                )}
+
+                <span>
+                  {status !== "" && status !== "done"
+                    ? "Running code..."
+                    : "Submit"}
+                </span>
               </button>
             </div>
             <Separator className="group h-0.5 my-0.75 self-stretch bg-transparent rounded-full hover:bg-monaco-muted active:bg-blue-500 transition-colors duration-150 cursor-col-resize flex items-center justify-center">
