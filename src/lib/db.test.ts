9bec2264127b5b838442eee7d4482663f9b42c29
lucasz26 <205996090+lucasz26@users.noreply.github.com>
2026-08-01 20:22:48 -0500

    Apply suggestions from code review
    Co-authored-by: marko <marko.pavicevic@outlook.com>

--- src/lib/db.test.ts
+++ src/lib/db.test.ts
@@ -1,5 +1,5 @@
 import { CodebookDatabaseAPI } from "@/lib/db.ts";
 
-test("placeholder test until we have a testing environmentexists", () => {
+test("placeholder test until we have a testing environment", () => {
   expect(true).toBe(true);
 });
