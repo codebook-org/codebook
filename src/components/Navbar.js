920be0665bd00e6863f441d3c54759f26813319b
marko <marko.pavicevic@outlook.com>
2026-08-26 13:04:37 -0700

    style: format

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,32 @@ import Link from "next/link";
 import Logo from "@/components/Logo";
 import Tooltip from "@/components/Tooltip";
 import UserMenu from "@/components/logincomponents/UserMenu";
-import { Home, Info, CircleQuestionMark, CodeXml, PencilLine, Settings } from "lucide-react";
+import {
+  Home,
+  Info,
+  CircleQuestionMark,
+  CodeXml,
+  PencilLine,
+  Settings,
+} from "lucide-react";
 import { useSession } from "next-auth/react";
 
 // general links for navigating codebook
 const NAV_LINKS_LEFT = [
   { href: "/", label: "Home", icon: Home, isExternal: false },
   { href: "/about", label: "About", icon: Info, isExternal: false },
-  { href: "/guide", label: "Guide", icon: CircleQuestionMark, isExternal: false },
-  { href: "https://github.com/codebook-org/codebook", label: "GitHub", icon: CodeXml, isExternal: true },
+  {
+    href: "/guide",
+    label: "Guide",
+    icon: CircleQuestionMark,
+    isExternal: false,
+  },
+  {
+    href: "https://github.com/codebook-org/codebook",
+    label: "GitHub",
+    icon: CodeXml,
+    isExternal: true,
+  },
 ];
 
 // additional links that require a user to be logged in
@@ -24,15 +41,11 @@ const NAV_LINKS_RIGHT = [
 export default function Navbar() {
   const { data: session, status, update } = useSession();
 
-
   return (
     <nav className="flex items-center justify-between p-4 sticky top-0 bg-background z-[100] overflow-visible h-16">
       <div className="flex items-center gap-4">
         <Link href="/" className="logo flex items-center">
-          <Logo 
-            className="mx-2" 
-            color="text-monaco-txt" 
-          />
+          <Logo className="mx-2" color="text-monaco-txt" />
           <div className="text-monaco-txt">codebook</div>
         </Link>
         <div className="w-[1px] h-4 bg-monaco-light mx-2" />
@@ -64,21 +77,23 @@ export default function Navbar() {
         ) : (
           <div className="flex items-center">
             <ul className="flex items-center gap-2 mr-2">
-              {NAV_LINKS_RIGHT.map(({ href, label, icon: Icon, isExternal }) => (
-                <li key={href}>
-                  <Tooltip content={label}>
-                    <Link
-                      href={href}
-                      target={isExternal ? "_blank" : undefined}
-                      rel={isExternal ? "noopener noreferrer" : undefined}
-                    >
-                      <div className=" group border-monaco-light p-2 rounded-lg hover:bg-monaco-mid">
-                        <Icon className="size-4.5 text-monaco-muted group-hover:text-monaco-txt" />
-                      </div>
-                    </Link>
-                  </Tooltip>
-                </li>
-              ))}
+              {NAV_LINKS_RIGHT.map(
+                ({ href, label, icon: Icon, isExternal }) => (
+                  <li key={href}>
+                    <Tooltip content={label}>
+                      <Link
+                        href={href}
+                        target={isExternal ? "_blank" : undefined}
+                        rel={isExternal ? "noopener noreferrer" : undefined}
+                      >
+                        <div className=" group border-monaco-light p-2 rounded-lg hover:bg-monaco-mid">
+                          <Icon className="size-4.5 text-monaco-muted group-hover:text-monaco-txt" />
+                        </div>
+                      </Link>
+                    </Tooltip>
+                  </li>
+                ),
+              )}
             </ul>
             <div className="w-[1px] h-4 bg-monaco-light mx-2" />
           </div>
