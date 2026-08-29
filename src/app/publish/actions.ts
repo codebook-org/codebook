3c69d2568990619d978b3eed897c4c8a0ba37b4c
marko <marko.pavicevic@outlook.com>
2026-08-28 23:45:29 -0700

    refactor: replace deprecated problems API function calls

--- src/app/publish/actions.ts
+++ src/app/publish/actions.ts
@@ -1,10 +1,8 @@
 "use server";
 import { CodebookDatabaseAPI } from "@/lib/db";
 
-// Stealing Markos idea of having an actions js. but it doesn't really affect how this works, aha..
-
 export async function addProblem(title, description, userId, starterCode) {
-  let data = await CodebookDatabaseAPI.createProblem({
+  let data = await CodebookDatabaseAPI.Problems.createProblem({
     title: title,
     description: description,
     userId: userId,
@@ -14,7 +12,7 @@ export async function addProblem(title, description, userId, starterCode) {
 }
 
 export async function addTestCasedb(problemId, input, expectedOut, visible) {
-  await CodebookDatabaseAPI.createTestCase({
+  await CodebookDatabaseAPI.Problems.TestCases.createTestCase({
     problemId: problemId,
     input: input,
     expectedOut: expectedOut,
