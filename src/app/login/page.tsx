bf232f0af7e02ec65c14e207473afef303f9d6b6
marko <marko.pavicevic@outlook.com>
2026-09-01 10:37:06 -0700

    style(login): fix sign in form margins

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,7 +18,7 @@ export default function LoginPage() {
 
   return (
     <div className="flex w-full h-full mt-16 flex-col items-center bg-background">
-      <div className="w-full max-w-md min-h-[425px] rounded-3xl border border-monaco-light bg-monaco-dark p-8 shadow-2xl backdrop-blur-md flex flex-col">
+      <div className="w-full max-w-md min-h-[410px] rounded-3xl border border-monaco-light bg-monaco-dark p-8 shadow-2xl backdrop-blur-md flex flex-col">
         <AnimatePresence mode="wait">
           <motion.div
             key={mode}
@@ -55,7 +55,7 @@ export default function LoginPage() {
             </div>
             {mode == "login" && (
               <div>
-                <div className="h-[1px] w-full my-6 bg-monaco-light" />
+                <div className="h-[1px] w-full mt-6 mb-9 bg-monaco-light" />
                 <GoogleSignIn />
               </div>
             )}
