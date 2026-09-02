514a0634871d191f8a8d5235f0046c1b554eb99e
marko <marko.pavicevic@outlook.com>
2026-09-02 10:19:21 -0700

    style:format

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -10,17 +10,14 @@ export default function HomeClient(): JSX.Element {
         {`> `}Welcome to codebook.
       </div>
       <div className="text-sm font-mono text-monaco-txt mt-4 w-180">
-        {`> `}codebook is a platform for hosting custom LeetCode-style programming problems. 
+        {`> `}codebook is a platform for hosting custom LeetCode-style
+        programming problems.
       </div>
       <div className="text-sm font-mono text-monaco-txt mt-4 w-180">
         {`> `}Find out more about codebook
         {
-          <Link
-            href="/about"
-          >
-            <button 
-              className="border border-yellow-600 hover:bg-yellow-600 cursor-pointer text-yellow-600 hover:text-monaco-txt transition-colors px-2 py-1 -my-1 rounded-lg ml-2"
-            >
+          <Link href="/about">
+            <button className="border border-yellow-600 hover:bg-yellow-600 cursor-pointer text-yellow-600 hover:text-monaco-txt transition-colors px-2 py-1 -my-1 rounded-lg ml-2">
               here
             </button>
           </Link>
@@ -28,14 +25,10 @@ export default function HomeClient(): JSX.Element {
         ,
       </div>
       <div className="text-sm font-mono text-monaco-txt mt-4 w-180">
-        {`> `}or visit our 
+        {`> `}or visit our
         {
-          <Link
-            href="/guide"
-          >
-            <button 
-              className="border border-green-600 hover:bg-green-600 cursor-pointer text-green-600 hover:text-monaco-txt transition-colors px-2 py-1 -my-1 rounded-lg mx-2"
-            >
+          <Link href="/guide">
+            <button className="border border-green-600 hover:bg-green-600 cursor-pointer text-green-600 hover:text-monaco-txt transition-colors px-2 py-1 -my-1 rounded-lg mx-2">
               Guide page
             </button>
           </Link>
@@ -43,17 +36,15 @@ export default function HomeClient(): JSX.Element {
         for comprehensive documentation of her features.
       </div>
       <div className="text-sm font-mono text-monaco-txt mt-4 w-180">
-        {`> `}Found a bug? Drop us a line on 
+        {`> `}Found a bug? Drop us a line on
         {
           <Link
             href="https://github.com/codebook-org/codebook"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <button 
-              className="border border-blue-500 hover:bg-blue-500 cursor-pointer text-blue-500 hover:text-monaco-txt transition-colors px-2 py-1 -my-1 rounded-lg ml-2"
-            >
-                GitHub
+            <button className="border border-blue-500 hover:bg-blue-500 cursor-pointer text-blue-500 hover:text-monaco-txt transition-colors px-2 py-1 -my-1 rounded-lg ml-2">
+              GitHub
             </button>
           </Link>
         }
