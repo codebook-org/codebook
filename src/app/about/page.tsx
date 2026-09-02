d6e4881565362926be2831a18d306ed10a95824a
Isabelle Nguyen <66228161+spinningincirclez@users.noreply.github.com>
2026-09-02 12:14:02 -0700

    feat(about): add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,36 @@
+export default async function AboutPage() {
+    return (
+      <main className="h-screen flex flex-col justify-center max-w-2xl mx-auto px-6 py-16 text-left">
+        <section className="mb-8">
+          <h1 className="text-monaco-txt font-mono text-2xl font-bold mb-3">
+            {`> `}about
+          </h1>
+          <p>
+            codebook is a collaborative coding platform designed to make technical interview prep and practice interactive.
+            unlike other traditional, solo-grind platforms, codebook lets you take the wheel, letting you create and publish your own problems.
+          </p>
+        </section>
+  
+        <section>
+          <h1 className="text-monaco-txt font-mono text-xl font-bold mb-3">
+            {`> `}features
+          </h1>
+          <ul className="list-disc pl-5 space-y-2">
+            <li>
+              <strong>community problems</strong> — write, test, and publish your
+              own coding problems and test cases.
+            </li>
+            <li>
+              <strong>competitive programming</strong> — an advanced alternative
+              to other platforms.
+            </li>
+            <li>
+              <strong>social outreach</strong> — access other profiles, share the
+              problems you create, or show off your trophy shelf of solved
+              problems!
+            </li>
+          </ul>
+        </section>
+      </main>
+    );
+  }
